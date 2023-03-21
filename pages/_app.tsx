cca284bd25dbc0e90eaf5d15dd5197ecd11d6c49
makylfang <makyl@newfang.io>
2023-03-21 11:51:27 +0400

    Added new wagmi version and @arcana/auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { wagmiClient } from "../utils/wagmi_client";
 import { WagmiConfig } from "wagmi";
+import { ArcanaConnector } from "@arcana/auth-wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+import { configureChains, createClient, Chain } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { getAuthProvider } from "../utils/getArcanaAuth";
+
+export const connector = (chains: Chain[]) => {
+  return new ArcanaConnector({
+    chains,
+    options: {
+      auth: getAuthProvider(),
+    },
+  });
+};
+
+const { chains, provider } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()]
+);
+
+export const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [connector(chains)],
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
